3755e7e47e1b1038d1ef095d515953cd4e1a0e24
Anthony Ware <aware91coding@gmail.com>
2020-05-01 21:53:35 -0500

    have added all the information I need. Working on bugs

--- src/components/Topping.js
+++ src/components/Topping.js
@@ -1,26 +1,80 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Toppings from './Toppings'
+import axios from 'axios';
+import * as yup from 'yup';
 
 const Topping = () => {
+    const [post, setPost] = useState([])
+    const [serverError, setServerError] = useState('')
+    const [formState, setFormState] = useState(initialFormState)
+    const [isButtonDisabled, seIsButtonDisabled] = useState(true)
+    const [error, setError] = useState(initialFormState)
+
     const initialFormState = {
-        position: '',
+        size: '',
         sauce: '',
         toppings: '',
         instructions: '',
         quantity: '',
-        number: '',
     }
 
-    
+    const validateChange = e => {
+        yup
+            .reach(formSchema, e.target.name)
+            .validate(e.target.value)
+            .then(valid => {
+                setErrors({ ...error, [e.target.name]: '' })
+            })
+            .catch(err => {
+                console.log('error', err);
+                setError({ ...errors, [e.target.name]: err.errors[0] })
+            })
+    }
+
+    useEffect(() => {
+        formSchema.isValid(formState).then(valid => {
+            console.log('valid', valid);
+            seIsButtonDisabled(!valid);
+        })
+    }, [formState])
+
+    const formSubmit = e => {
+        e.preventDefault();
+        axios
+            .post("https://reqres.in/api/users", formState)
+            .then(res => {
+            setPost(res.data);
+            console.log('success', post)
+            setFormState({
+            size: '',
+            sauce: '',
+            toppings: '',
+            instructions: '',
+            quantity: '',
+            });
+            setServerError(null);
+            })
+            .catch(err => {
+            setServerError("oops! something happened!");
+        });
+    };
+
+    const formSchema = yup.object().shape({
+        size: yup.string().required('Must Choose a Size'),
+        sauce: yup.string().required('Must Choose a Sauce'),
+        instructions: yup.boolean().oneOf([true], 'Please Choose a Topping'),
+        quantity: yup.string().required(),
+    })
 
     return (
-        <div>
+        <form onSubmit={formSubmit}>
+            {serverError ? <p className='error'>{serverError}</p> : null}
             <h3>Build Your Own Pizza</h3>
             <div>
                 <h4>Choice of Size</h4>
-                <label htmlFor='positions'>
+                <label htmlFor='size'>
                     Required
-                    <select id='positions' name='positions' onChange={inputChange}>
+                    <select id='size' name='size' onChange={inputChange}>
                         <option value=''>Select</option>
                         <option value='small'>Small</option>
                         <option value='medium'>Medium</option>
@@ -30,16 +84,16 @@ const Topping = () => {
                 <fieldset>
                     <legend>Choice of Sauce</legend>
                     <p>Required</p>
-                    <input type='radio' id='original' name='sauce'>
+                    <input type='radio' id='original' name='sauce' value='original'>
                         <label for='original'>Original Red</label>
                     </input><br></br>
-                    <input type='radio' id='garlic' name='sauce'>
+                    <input type='radio' id='garlic' name='sauce' value='garlic'>
                         <label for='garlic'>Garlic Ranch</label>
                     </input><br></br>
-                    <input type='radio' id='bbq' name='sauce'>
+                    <input type='radio' id='bbq' name='sauce' value='bbq'>
                         <label for='bbq'>BBQ Sauce</label>
                     </input><br></br>
-                    <input type='radio' id='spinach' name='sauce'>
+                    <input type='radio' id='spinach' name='sauce' value='spinach'>
                         <label for='spinach'>Spinach Alfredo</label>
                     </input>
                 </fieldset>
@@ -62,13 +116,15 @@ const Topping = () => {
                         id='quantity' 
                         name='quantity' 
                         min='1' 
-                        max='20' />
+                        max='20'
+                        value='1'
+                        />
                 </label>
                 <button disabled={isButtonDisabled} type='submit'>
                     Add to Order
                 </button>
             </div>
-        </div>
+        </form>
     )
 }
 
